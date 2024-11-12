58df2a1645fb13ce70e51705499543ceb8404e3c
Jain Joseph <101648281+jainkjoseph@users.noreply.github.com>
2024-11-12 17:17:22 +0400

    Add files via upload

--- masterHead.py
+++ masterHead.py
@@ -31,7 +31,7 @@ conn.commit()
 xx = "SELECT acc_name FROM  master WHERE acc_name IS NOT NULL"
 c.execute(xx)
 found_record = c.fetchone()
-// for voucher type selection
+#for voucher type selection
 if found_record is None:
     mtext = [("ASSETS", 1, 3, 0, ""), ("LIABILITIES", 1, 1, 0, ""), ("EXPENSES", 1, 1, 0, ""), ("INCOME", 1, 1, 0, ""),
              ("CASH BOOK", 2, 0, 1, "ASSETS"),
@@ -54,7 +54,7 @@ for row in results:
 
 
 def do_Save(*arg):
-    # pass
+    tlevel = 0.00
 
     c.execute("CREATE TABLE IF NOT EXISTS Master(acc_code INT(5) NOT NULL AUTO_INCREMENT PRIMARY KEY ,"
               "acc_name VARCHAR(35),group_name VARCHAR(35),group_code INT(5), op_bal FLOAT(12,2),dr_cr VARCHAR(2), "
@@ -63,6 +63,7 @@ def do_Save(*arg):
     print("Master table successfully created")
     macc_name = acc_name.get()
     mgroup_name = group_name.get()
+
     mop_bal = opn_bal.get()
     if mop_bal == "":
         mop_bal = float()
@@ -74,6 +75,7 @@ def do_Save(*arg):
         mlevel = 2
         mdr_cr = dr_cr.get()
     # head creation write to file
+
     master_data = (mgroup_code, mgroup_name, macc_name, mop_bal, mdr_cr, mlevel, 0)
     mysql_insert_query = (
         "INSERT INTO master(group_code,group_name,acc_name,op_bal,dr_cr,level,child) VALUES(%s,%s,%s,%s,%s,%s,%s)")
@@ -87,7 +89,7 @@ def do_Save(*arg):
 
     c5.execute(query, (mgroup_code,))
     rows = c5.fetchall()
-    print(rows)
+
     for i in rows:
         tlevel = i[7]
     xlevel = tlevel + 1
@@ -111,18 +113,39 @@ def do_Reset():
 
 
 def my_upd(*args):
-    global mgroup_code
-    query = "SELECT acc_code,acc_name FROM  master"
-    my_data = c.execute(query)
+    global mgroup_code,mgroup_name
+    mgroup_name = group_name.get()
+    print(mgroup_name)
+    c = conn.cursor()
+
+    #query = "SELECT acc_name FROM  master WHERE acc_name = %s"
+    query = "SELECT * FROM master WHERE acc_name = %s"
+    my_data = c.execute(query,(mgroup_name,))
     results = c.fetchall()
+    print("result")
+    print(results)
     my_list = results
     for row in results:
         if row[1] == sel.get():
             mgroup_code = row[0]
+            print(mgroup_code)
 
+"""
+def lookup_Cash_Bank(event):
+    global macc_code, macc_name, mgroup_name
+    cash_Bank_Name = acc_name.get()
 
-#   print(mgroup_code)
+    c3 = conn.cursor()
+    query = "SELECT * FROM master WHERE acc_name = %s"
+    c3.execute(query, (cash_Bank_Name,))
 
+    rows = c3.fetchall()
+
+    for i in rows:
+        macc_code = i[0]
+        macc_name = i[1]
+        mgroup_name = i[2]
+"""
 master = tk.Tk()
 master.geometry("650x150")
 tk.Label(master, text="Account Name", font=('Ariel', 14)).grid(row=0)
@@ -134,17 +157,16 @@ mgroup_code = 0
 box_value = tk.StringVar()
 sel = tk.StringVar()
 acc_name = tk.Entry(master, width=30, font=('Ariel', 14))
-
-group_name = ttk.Combobox(master, values=my_list, textvariable=sel)
-opn_bal = tk.Entry(master, width=12, font=('Ariel', 14), justify=RIGHT)
-dr_cr = ttk.Combobox(master, textvariable=box_value, values=["", "Dr", "Cr"], width=10, font=('Ariel', 14))
-
 acc_name.grid(row=0, column=1)
+group_name = ttk.Combobox(master, values=my_list, textvariable=sel)
 group_name.grid(row=1, column=1)
+group_name.bind('<<ComboboxSelected>>', my_upd)
+opn_bal = tk.Entry(master, width=12, font=('Ariel', 14), justify=RIGHT)
 opn_bal.grid(row=2, column=1)
+dr_cr = ttk.Combobox(master, textvariable=box_value, values=["Dr", "Cr"], width=10, font=('Ariel', 14))
 dr_cr.grid(row=2, column=2)
-sel.trace('w', my_upd)
-# obj = Master(acc_name,group_name,mgroup_code,opn_bal,dr_cr)
+
+#obj = Master(acc_name,group_name,mgroup_code,opn_bal,dr_cr)
 b1 = tk.Button(frame1, text='OK', font=('Ariel', 14), command=do_Save)
 b2 = tk.Button(frame1, text='RESET', font=('Ariel', 14), command=lambda: do_Reset())
 b3 = tk.Button(frame1, text='QUIT', font=('Ariel', 14), command=lambda: do_Exit())
