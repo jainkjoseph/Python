f4d78220dafa4eb30f6268f63bc41a4e02abc2be
Jain Joseph <101648281+jainkjoseph@users.noreply.github.com>
2024-11-18 17:09:38 +0400

    Add files via upload

--- CasbBankReport.py
+++ CasbBankReport.py
@@ -14,14 +14,11 @@ from tkinter.ttk import Label, LabelFrame
 
 from tkcalendar import Calendar, DateEntry
 
-
-import mysql.connector
-
 from datetime import date
 
 from ReportLedger import *
 
-
+import mysql.connector
 
 conn = mysql.connector.connect(
     host='localhost',
@@ -38,76 +35,82 @@ options = []
 #c.execute("SELECT acc_code,acc_name FROM  master WHERE acc_name = 'CASH BOOK' OR  group_name = '{BANK ACCOUNTS}' ")
 c.execute("SELECT acc_code,acc_name FROM  master")
 
-display_Cash_Bank = c.fetchall() # display cash & Banks head only
+display_Cash_Bank = c.fetchall()  # display cash & Banks head only
 for i in display_Cash_Bank:
     options.append(str(i[1]))
 
-master = tk.Tk()
-master.geometry("550x250")
-master.title("Home Expenses", )
-# This varibles for Cash/Bank master file storing
-macc_code =  StringVar()
-macc_name = StringVar()
 
-def lookup_Cash_Bank(event):
-    global macc_name,macc_code
-    cash_Bank_Name = acc_name.get()
+def lookup_cash_bank(event):
+    global macc_name, macc_code
+    cash_Bank_Name = master.acc_name.get()
+
     c3 = conn.cursor()
     query = "SELECT * FROM master WHERE acc_name = %s"
-    c3.execute(query,(cash_Bank_Name,))
+    c3.execute(query, (cash_Bank_Name,))
 
     rows = c3.fetchall()
 
     for i in rows:
-        macc_code=i[0]
-        macc_name=i[1]
+        macc_code = i[0]
+        macc_name = i[1]
+
 
 def do_print():
-    #print(macc_code)
-    ReportLedger(macc_code,macc_name)
-   # pass
+    ReportLedger1(macc_code, macc_name)
 
 
 def do_Exit():
     quit()
 
+
 def do_Reset():
     macc_code = ""
-    acc_name.delete(0,END)
+    acc_name.delete(0, END)
     acc_name.focus()
 
-sel1 = tk.StringVar()
-label_Color = "Blue"
-label_frame0 = tk.LabelFrame(master, text="CASH/BANK REGISTERS", font=('Cooper Black',18,'italic') ,height=125,width=700,labelanchor='n' )
-label_frame0.pack(padx=5,pady=5)
-Label(label_frame0, text="Cash/Bank", font=('Ariel', 14),foreground='Blue').place(x=25, y=20)
-acc_name = ttk.Combobox(label_frame0,values=options,textvariable=sel1,font=('Ariel', 14),width=25)
-(acc_name.place(y=20,x=130))
-""" place without next line; bind showing nontype error
-"""
 
-acc_name.bind('<<ComboboxSelected>>', lookup_Cash_Bank)
+def main(master):
+    # master = tk.Tk()
 
-Label(label_frame0, text="period", font=('Ariel', 14),foreground='Blue').place(x=25, y=60)
-today= date.today()
-doc_Date1 = today.strftime("%d/%m/%Y")
+    master.geometry("550x250")
+    master.title("Home Expenses", )
+    # This varibles for Cash/Bank master file storing
+    master.macc_code = StringVar()
+    master.macc_name = StringVar()
+    sel1 = tk.StringVar()
+    label_Color = "Blue"
+    label_frame0 = tk.LabelFrame(master, text="CASH/BANK REGISTERS", font=('Cooper Black', 18, 'italic'), height=125,
+                                 width=700, labelanchor='n')
+    label_frame0.pack(padx=5, pady=5)
+    Label(label_frame0, text="Cash/Bank", font=('Ariel', 14), foreground='Blue').place(x=25, y=20)
+    master.acc_name = ttk.Combobox(label_frame0, values=options, textvariable=sel1, font=('Ariel', 14), width=25)
 
-doc_Date2 = date.today()
+    master.acc_name.current(0)
+    master.acc_name.place(y=20, x=130)
+    """ place without next line; bind showing nontype error
+   """
 
-doc_Date1 = DateEntry(label_frame0, font=('Ariel', 14),width=10)
-doc_Date1.place(x=130,y=60)
-doc_Date2 = DateEntry(label_frame0, font=('Ariel', 14),width=10)
-doc_Date2.place(x=300,y=60)
+    #master.acc_name.bind('<<ComboboxSelected>>',comboclick)
+    master.acc_name.bind('<<ComboboxSelected>>', lookup_cash_bank)
+    Label(label_frame0, text="period", font=('Ariel', 14), foreground='Blue').place(x=25, y=60)
+    today = date.today()
+    master.doc_Date1 = today.strftime("%dd/%mm/%YY")
+    master.doc_Date2 = date.today()
+    doc_Date1 = DateEntry(label_frame0, font=('Ariel', 14), width=10)
+    doc_Date1.place(x=130, y=60)
+    doc_Date2 = DateEntry(label_frame0, font=('Ariel', 14), width=10)
+    doc_Date2.place(x=300, y=60)
+    label_frame2 = tk.LabelFrame(master, height=80, width=700, labelanchor='n')
+    label_frame2.pack(padx=5, pady=5)
+    b1 = tk.Button(label_frame2, text='OK', font=('Ariel', 14), fg=label_Color, padx=20, command=do_print)
+    b2 = tk.Button(label_frame2, text='RESET', font=('Ariel', 14), fg=label_Color, padx=20, command=lambda: do_Reset())
+    b3 = tk.Button(label_frame2, text='QUIT', font=('Ariel', 14), fg=label_Color, padx=20, command=lambda: do_Exit())
+    b1.place(y=25, x=100)
+    b2.place(y=25, x=250)
+    b3.place(y=25, x=400)
+    master.mainloop()
 
 
-label_frame2 = tk.LabelFrame(master, height=80,width=700,labelanchor='n' )
-label_frame2.pack(padx=5,pady=5)
-b1 = tk.Button(label_frame2, text='OK', font=('Ariel', 14), fg=label_Color, padx=20, command=do_print )
-b2 = tk.Button(label_frame2, text='RESET', font=('Ariel', 14), fg=label_Color, padx=20, command=lambda: do_Reset())
-b3 = tk.Button(label_frame2, text='QUIT', font=('Ariel', 14), fg=label_Color, padx=20, command=lambda: do_Exit())
-
-b1.place(y=25, x=100)
-b2.place(y=25, x=250)
-b3.place(y=25, x=400)
-
-master.mainloop()
+if __name__ == "__main__":
+    master = tk.Tk()
+    main(master)
--- ReportLedger.py
+++ ReportLedger.py
@@ -3,40 +3,33 @@ from reportlab.lib import colors
 
 from reportlab.lib.pagesizes import letter
 
-from reportlab.platypus import SimpleDocTemplate, Table,TableStyle
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
 
 import mysql.connector
 
-def ReportLedger(macc_code,head,mop_bal):
+
+def ReportLedger1(macc_code, head):
     tcode = macc_code
-    thead = head
-    top_bal = mop_bal
-    print(top_bal)
 
     conn = mysql.connector.connect(
         host='localhost',
         user='root',
-        passwd="Milan@2000",
+        passwd="",
         database='accounts')
     c = conn.cursor()
     conn.commit()
     pdf = SimpleDocTemplate("Reports.pdf", pagesize=letter)
 
     #c.execute("SELECT doc_date,doc_no,ledger_name, ledger_dr_amount, ledger_cr_amount FROM  Transanction WHERE acc_code =macc_code")
-    query =("SELECT doc_date,doc_no,ledger_name, ledger_dr_amount, ledger_cr_amount FROM  Transanction WHERE acc_code "
-            "= %s")
+    query = ("SELECT doc_date,doc_no,ledger_name, ledger_dr_amount, ledger_cr_amount FROM  Transanction WHERE acc_code "
+             "= %s")
 
+    c.execute(query, (tcode,))
 
-    c.execute(query,(tcode,))
-
-
-    data=[]
+    data = []
     elements = []
-  #  titles1 = ['Date','Voucher No.','Description','Amount','Amount','Balance']
-  #  data.append(titles1)
-  
 
-    titles = ['Date','Voucher No.','Description','Amount','Amount','Balance']
+    titles = ['Date', 'Voucher No.', 'Description', 'Amount', 'Amount', 'Balance']
 
     data.append(titles)
 
@@ -44,14 +37,13 @@ def ReportLedger(macc_code,head,mop_bal):
     for p in data1:
         data.append(p)
 
-    total_dr_amount = mop_bal+ sum(row[3] for row in data[1:]) #  ledger_dr_amount in4th position
-    total_cr_amount = sum(row[4] for row in data[1:]) #  ledger_cr_amount in5th position
-    data.append([' ',' ',' ',"-----------","----------"])
-    data.append([' ',' ','Total =',round(total_dr_amount,2) , round(total_cr_amount,2), round(total_dr_amount-total_cr_amount,2)])
-    data.append([' ',' ',' ',"=======","======"])
+    total_dr_amount = sum(row[3] for row in data[1:])  #  ledger_dr_amount in4th position
+    total_cr_amount = sum(row[4] for row in data[1:])  #  ledger_cr_amount in5th position
+    data.append([' ', ' ', ' ', "-----------", "----------"])
+    data.append([' ', ' ', 'Total =', round(total_dr_amount, 2), round(total_cr_amount, 2),
+                 round(total_dr_amount - total_cr_amount, 2)])
+    data.append([' ', ' ', ' ', "=======", "======"])
     table = Table(data)
 
     elements.append(table)
     pdf.build(elements)
-
-
