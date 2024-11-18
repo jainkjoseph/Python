365fdeb52749cd6db0d98a23c111a450bc038525
Jain Joseph <101648281+jainkjoseph@users.noreply.github.com>
2024-11-18 17:44:54 +0400

    Add files via upload

--- CasbBankReport.py
+++ CasbBankReport.py
@@ -69,9 +69,7 @@ def do_Reset():
     acc_name.focus()
 
 
-def main(master):
-    # master = tk.Tk()
-
+def main():
     master.geometry("550x250")
     master.title("Home Expenses", )
     # This varibles for Cash/Bank master file storing
@@ -102,15 +100,15 @@ def main(master):
     doc_Date2.place(x=300, y=60)
     label_frame2 = tk.LabelFrame(master, height=80, width=700, labelanchor='n')
     label_frame2.pack(padx=5, pady=5)
-    b1 = tk.Button(label_frame2, text='OK', font=('Ariel', 14), fg=label_Color, padx=20, command=do_print)
+    b1 = tk.Button(label_frame2, text='Generate PDF', font=('Ariel', 14), fg=label_Color, padx=20, command=do_print)
     b2 = tk.Button(label_frame2, text='RESET', font=('Ariel', 14), fg=label_Color, padx=20, command=lambda: do_Reset())
     b3 = tk.Button(label_frame2, text='QUIT', font=('Ariel', 14), fg=label_Color, padx=20, command=lambda: do_Exit())
-    b1.place(y=25, x=100)
-    b2.place(y=25, x=250)
-    b3.place(y=25, x=400)
+    b1.place(y=20, x=50)
+    b2.place(y=20, x=250)
+    b3.place(y=20, x=400)
     master.mainloop()
 
 
 if __name__ == "__main__":
     master = tk.Tk()
-    main(master)
+    main()
